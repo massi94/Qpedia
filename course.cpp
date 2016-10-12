4d70bdd3a01d6cf7e8320bb1bed35ce5f25b0707
massi94 <massignanfab@gmail.com>
2016-10-12 21:49:08 +0200

    Inizio implementazione metodi di course

--- course.cpp
+++ course.cpp
@@ -2,45 +2,54 @@
 
 course::course(QString n) : name(n){}
 
-QString course::getMateria() const {return name;}
+QString course::getMateria() const {
+    return name;
+}
 
-QVector<const note*> course::getNotes() const {return noteList;}
+list<const note*> course::getNotes() const {
+    return noteList;
+}
 
-QVector<const note*> course::getDefinitions() const {
-    QVector<const note*> ris;
-    QVector<const note*>::const_iterator it;
+list<const note*> course::getDefinitions() const {
+    list<const note*> ris;
+    list<const note*>::iterator it;
 
     for(it=noteList.begin();it!=noteList.end();++it)
     {
-        const definition * d=dynamic_cast<const definition*>(*it);
+        const definition * d=dynamic_cast<const definition*>(noteList[it]);
         if(d) ris.push_front(d);
     }
     return ris;
 }
 
-QVector<const note*> course::getSchemas() const {
-    QVector<const note*> ris;
-    QVector<const note*>::const_iterator it;
+list<const note*> course::getSchemas() const {
+    list<const note*> ris;
+    list<const note*>::iterator it;
 
     for(it=noteList.begin();it!=noteList.end();++it)
     {
-        const schema * d=dynamic_cast<const schema*>(*it);
+        const schema * d=dynamic_cast<const schema*>(noteList[it]);
         if(d) ris.push_front(d);
     }
     return ris;
 }
 
-QVector<const note*> course::getSources() const {
-    QVector<const note*> ris;
-    QVector<const note*>::const_iterator it;
+list<const note*> course::getSources() const {
+    list<const note*> ris;
+    list<const note*>::iterator it;
 
     for(it=noteList.begin();it!=noteList.end();++it)
     {
-        const source * d=dynamic_cast<const source*>(*it);
+        const source * d=dynamic_cast<const source*>(noteList[it]);
         if(d) ris.push_front(d);
     }
     return ris;
 }
 
-void course::setName(const QString & n){name=n;}
+void course::setName(const QString & n){
+    name=n;
+}
 
+void course::addNote(const note* app){
+    noteList.push_back(app);
+}
--- course.h
+++ course.h
@@ -2,28 +2,28 @@
 #define COURSE_H
 
 #include <QString>
-#include <QVector>
 
 #include"dbio.h"
 #include"note.h"
 #include"definition.h"
 #include"schema.h"
 #include"source.h"
+#include"list.h"
 
 
 class course
 {
 private:
     QString name;
-    QVector<const note*> noteList;
+    list<const note*> noteList;
 public:
     course(QString);
 
     QString getMateria() const;
-    QVector<const note*> getNotes() const;
-    QVector<const note*> getDefinitions() const;
-    QVector<const note*> getSchemas() const;
-    QVector<const note*> getSources() const;
+    list<const note*> getNotes() const;
+    list<const note*> getDefinitions() const;
+    list<const note*> getSchemas() const;
+    list<const note*> getSources() const;
 
     void setName(const QString&);
 
